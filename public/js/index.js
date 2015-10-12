731ef0c70154fdfa599fb284f65af53bfe719d0a
Joseph Milla <jjtmilla@gmail.com>
2015-10-12 09:45:11 -0500

    Add footer

--- public/js/index.js
+++ public/js/index.js
@@ -6,34 +6,34 @@ var controller = new ScrollMagic.Controller({
 
 
 var pinani = new TimelineMax()
-    .add(TweenMax.to("#wipe", 1, {transform: "translateY(0)"}))
-    .add(TweenMax.to("#second-wipe", 1, {transform: "translateY(0)"}))
-    .add(TweenMax.to("#slide", 1, {top: "0%", ease: Bounce.easeOut, delay: 0.2}))
+    .add(TweenMax.to("#wipe", 1.5, {transform: "translateY(0)"}))
+    .add(TweenMax.to("#second-wipe", 1.5, {transform: "translateY(0)"}))
+    .add(TweenMax.to("#slide", 1.5, {top: "0%", ease: Bounce.easeOut, delay: 0.2}))
     .add([
-        TweenMax.to("#slide h1:first-child", 0.2, {autoAlpha: 0}),
-        TweenMax.from("#slide h1:last-child", 0.2, {autoAlpha: 0})
+        TweenMax.to("#slide h1:first-child", 0.7, {autoAlpha: 0}),
+        TweenMax.from("#slide h1:last-child", 0.7, {autoAlpha: 0})
     ])
     .add([
-        TweenMax.to("#slide", 0.3, {backgroundColor: "yellow"}),
-        TweenMax.to("#slide h1:last-child", 0.3, {color: "blue"})
+        TweenMax.to("#slide", 0.8, {backgroundColor: "yellow"}),
+        TweenMax.to("#slide h1:last-child", 0.8, {color: "blue"})
     ])
     .add([
-        TweenMax.to("#slide", 0.3, {backgroundColor: "green"}),
-        TweenMax.to("#slide h1:last-child", 0.3, {color: "red"})
+        TweenMax.to("#slide", 0.8, {backgroundColor: "green"}),
+        TweenMax.to("#slide h1:last-child", 0.8, {color: "red"})
     ])
     .add([
-        TweenMax.to("#slide", 0.3, {backgroundColor: "red"}),
-        TweenMax.to("#slide h1:last-child", 0.3, {color: "white"})
+        TweenMax.to("#slide", 0.8, {backgroundColor: "red"}),
+        TweenMax.to("#slide h1:last-child", 0.8, {color: "white"})
     ])
     .add([
-        TweenMax.to("#slide", 0.3, {backgroundColor: "#c7e1ff"}),
-        TweenMax.to("#slide h1:last-child", 3, {color: "black"})
+        TweenMax.to("#slide", 0.8, {backgroundColor: "#c7e1ff"}),
+        TweenMax.to("#slide h1:last-child", 8, {color: "black"})
     ])
-    .add(TweenMax.to("#slide-uno", 1, {transform: "translateX(0)"}))
-    .add(TweenMax.to("#third-wipe", 1, {transform: "translateY(0)"}))
-    .add(TweenMax.to("#slide-dos", 1, {transform: "translateX(0)"}))
-    .add(TweenMax.to("#fourth-wipe", 1, {transform: "translateY(0)"}))
-    .add(TweenMax.from("#unpin", .5, {top: "100%"}));
+    .add(TweenMax.to("#slide-uno", 1.5, {transform: "translateX(0)"}))
+    .add(TweenMax.to("#third-wipe", 1.5, {transform: "translateY(0)"}))
+    .add(TweenMax.to("#slide-dos", 1.5, {transform: "translateX(0)"}))
+    .add(TweenMax.to("#fourth-wipe", 1.5, {transform: "translateY(0)"}))
+    .add(TweenMax.from("#unpin", 1, {top: "100%"}));
 
 new ScrollMagic.Scene({
   triggerElement: "section#pin",
