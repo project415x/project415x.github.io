4fc0e32cb9bd08baf9371ce8292d3e1400e7fb41
Joseph Milla <jjtmilla@gmail.com>
2015-10-19 10:41:42 -0500

    Refactor

--- public/js/canvas2.js
+++ public/js/canvas2.js
@@ -220,6 +220,7 @@ var dashItem;
 
 // function onMouseDown(event)
 
+// Convert Screen Coordinates to Math Coordinates
 function convertToMathCoords(x, y) {
   var newX = (x - 250)/25;
   var newY = -(y - 250)/25;
@@ -227,6 +228,7 @@ function convertToMathCoords(x, y) {
   return [newX, newY];
 }
 
+// Convert the Math Coordinates back to Screen Coordinates
 function convertToScreenCoords(x, y) {
   var newX = 25*x + 250;
   var newY = -25*y + 250;
@@ -234,6 +236,11 @@ function convertToScreenCoords(x, y) {
   return [newX, newY];
 }
 
+// Apply Matrix
+function applyMatrix(x, y, matrix) {
+  var matrixApplied = [matrix[0][0]*x + matrix[0][1]*y, matrix[1][0]*x + matrix[1][1]*y];
+  return matrixApplied;
+}
 
 function onMouseDown(event) {
   console.log("Weeee");
@@ -245,15 +252,14 @@ function onMouseDown(event) {
   /* Same as input */
   // to = new Point(250 + input.x, 250 + input.y);
 
-  /* New Formula */
-  // to = new Point(2*input.x - input.y - 500, input.x - input.y + 250)
-
-  /* New Formula 2 */
+  /* New Formula  */
+  var matrix = [[2, 1], [1, -1]];
   var mathCoords = convertToMathCoords(250 + input.x, 250 + input.y);
-  var matrixApplied = [2*mathCoords[0] + 1*mathCoords[1], 1*mathCoords[0] + -1*mathCoords[1]];
+  var matrixApplied = applyMatrix(mathCoords[0], mathCoords[1], matrix);
   var screenCoords = convertToScreenCoords(matrixApplied[0], matrixApplied[1]);
   to = new Point(screenCoords[0], screenCoords[1]);
 
+  /* Debug */
   // console.log("input.x: " + input.x + ", input.y: " + input.y)
   // console.log("new.x: " + (250 + input.x) + ", new.y: " + (250 + input.y))
   // console.log("to: " + to);
