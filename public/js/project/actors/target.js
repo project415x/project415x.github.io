6a19f4c03c4676e0bda341b118369adbfe29eb30
cmalk <cmalkiewich1@gmail.com>
2016-01-29 18:29:03 -0600

    deleted extraneous files and moved actors.js into vector.js and target.js

---
+++ public/js/project/actors/target.js
@@ -0,0 +1,31 @@
+/**
+* TARGET CONSTRUCTOR
+*
+  var settings = {
+	x: 2,
+	y: 2,
+	ttl: 30	
+  };
+*
+*/
+
+function target(settings) {
+
+	this.x = settings.x;
+	this.y = settings.y;
+	this.ttl = settings.ttl;
+	this.color = '#FF0000';
+
+	// also include drawing object
+	this.drawingObject = [];
+
+	function update(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	function updateColor(dist) {
+		this.color = '#FF0000';
+		// TODO: make not dumb
+	}
+}
--- public/js/project/actors/vector.js
+++ public/js/project/actors/vector.js
@@ -1,11 +1,16 @@
-function vector() {
+function vector(x,y) {
 
 	this.x = x;
 	this.y = y;
+
+	this.drawingObject = [];
 
 	function updateVector(x,y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	function copy(x,y) {
+
+	}
 }
