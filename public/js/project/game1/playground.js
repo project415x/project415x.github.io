428b752560203a7fea66bed479cab3e932b15a58
Skam <cdart2@illinois.edu>
2016-03-12 18:11:03 -0600

    inserted playground inside of level 1.

--- public/js/project/game1/playground.js
+++ public/js/project/game1/playground.js
@@ -23,12 +23,39 @@ function initPlayground() {
 
 	// generate target(s)
 	outputTarget.init()
-	console.log(outputVector.color)
 }
 
-function startPlayground() {
+
+// think of this as the main function :)
+startPlayground = function startPlayground() {
 	initPlayground();
 }
 
-// think of this as the main function :)
-startPlayground();
+module.exports = {
+
+	initPlayground: function() {
+		// Create objects needed for game
+		var inputCanvas = new Canvas(config.inputCanvasSettings),
+				inputVector = new Vector(config.inputVectorSettings),
+				outputVector = new Vector(config.outputVectorSettings),
+				outputCanvas = new Canvas(config.outputCanvasSettings),
+				outputTarget = new Target(config.targetSettings);
+
+		// draw grid(s)
+		inputCanvas.drawCanvas();
+		outputCanvas.drawCanvas();
+		outputCanvas.drawProgressBar();
+
+		// draw vector(s)
+		inputVector.init();
+		outputVector.init();
+
+		// generate target(s)
+		outputTarget.init()
+	},
+
+	startPlayground: function() {
+		this.initPlayground();
+	}
+	
+};
