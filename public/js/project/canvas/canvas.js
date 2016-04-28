7e00a3b31e15f76cc945432b906bb4c289b6c0ca
Skam <cdart2@illinois.edu>
2016-04-27 21:13:12 -0500

    white background to transparent

--- public/js/project/canvas/canvas.js
+++ public/js/project/canvas/canvas.js
@@ -34,10 +34,10 @@ function Canvas(settings) {
   this.minY = settings.minY || -10,
   this.maxX = settings.maxX || 10,
   this.maxY = settings.maxY || 10,
-  this.pixelWidth = settings.pixelWidth || 500,
-  this.pixelHeight = settings.pixelHeight || 500,
-  this.originX = ( this.pixelWidth * (-this.minX)/(this.maxX - this.minX)) || 250,
-  this.originY = ( this.pixelHeight * (-this.minY)/(this.maxY - this.minY)) || 250,
+  this.pixelWidth = settings.pixelWidth || 400,
+  this.pixelHeight = settings.pixelHeight || 400,
+  this.originX = ( this.pixelWidth * (-this.minX)/(this.maxX - this.minX)) || 200,
+  this.originY = ( this.pixelHeight * (-this.minY)/(this.maxY - this.minY)) || 200,
   this.origin = {
     x: this.originX,
     y: this.originY
